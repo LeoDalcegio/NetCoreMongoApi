1b3cd95682bd9fe8fa95339a59261725d1bd53e6
leonardod alcegio <leoodalcegio@hotmail.com>
2020-11-11 23:19:09 -0300

    primeiro commit

---
+++ Data/MongoDbContext.cs
@@ -0,0 +1,45 @@
+using System;
+using MongoApi.Data.Collections;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace MongoApi.Data
+{
+    public class MongoDbContext
+    {
+        public IMongoDatabase DB { get; }
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            try
+            {
+                var settings = MongoClientSettings.FromUrl(new MongoUrl(configuration["ConnectionString"]));
+                var client = new MongoClient(settings);
+
+                DB = client.GetDatabase(configuration["NomeBanco"]);
+                MapClasses();
+            }
+            catch (Exception ex)
+            {
+                throw new MongoException("It was not possible to connect to MongoDB", ex);
+            }
+        }
+
+        private void MapClasses()
+        {
+            var conventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+            ConventionRegistry.Register("camelCase", conventionPack, t => true);
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Infectado)))
+            {
+                BsonClassMap.RegisterClassMap<Infectado>(i =>
+                {
+                    i.AutoMap();
+                    i.SetIgnoreExtraElements(true);
+                });
+            }
+        }
+    }
+}
